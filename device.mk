a6b05031d54b48c24d52317f541f80f643527fca
Itz-Kanasama <164613153+Itz-Kanasama@users.noreply.github.com>
2025-04-22 19:50:47 +0000

    vince: drop libmm-qcamera & libdng_sdk.vendor
    Co-authored-by: RennAlt <shenprjktplayground@gmail.com>

--- device.mk
+++ device.mk
@@ -26,9 +26,7 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    camera.msm8953 \
-    libmm-qcamera \
-    libdng_sdk.vendor
+    camera.msm8953
 
 # Consumer IR
 PRODUCT_PACKAGES += \
