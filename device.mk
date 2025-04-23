48d5fd0c97bba673884515ed8ef1e5a92bbcf767
RenzAlt <129452476+RennAlt@users.noreply.github.com>
2025-04-23 16:47:37 -0400

    vince: Build missing libraries for 14 QPR3
    and other missing

--- device.mk
+++ device.mk
@@ -34,7 +34,10 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.xiaomi
+    android.hardware.biometrics.fingerprint@2.3-service.xiaomi \
+    com.fingerprints.extension@1.0.vendor \
+    vendor.goodix.hardware.fingerprint@1.0.vendor \
+    liblzma.vendor
 
 PRODUCT_PACKAGES += \
      liblzma.vendor:64
