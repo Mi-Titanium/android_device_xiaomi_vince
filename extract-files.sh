e81b0c85ca07380928cce7386eaf1c3ef76521e8
Itz-Kanasama <164613153+Itz-Kanasama@users.noreply.github.com>
2025-04-23 00:34:23 +0000

    vince: switch to patchelf v0.18 for patching fingerprint blobs
    Co-authored-by: RennAlt <shenprjktplayground@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -9,26 +9,23 @@
 function blob_fixup() {
     case "${1}" in
         vendor/lib64/libvendor.goodix.hardware.fingerprint@1.0-service.so)
-            "${PATCHELF_0_8}" --remove-needed "libprotobuf-cpp-lite.so" "${2}"
+            "${PATCHELF_0_18}" --remove-needed "libprotobuf-cpp-lite.so" "${2}"
             ;;
         vendor/lib64/libgf_hal.so)
             # NOP gf_hal_test_notify_acquired_info()
             "${SIGSCAN}" -p "10 03 00 d0 11 52 46 f9" -P "10 03 00 d0 1f 20 03 d5" -f "${2}"
             ;;
-        vendor/lib/libchromaflash.so)
-            ;&
-        vendor/lib/libseemore.so)
-            ;&
-        vendor/lib/liboptizoom.so)
-            ;&
-        vendor/lib/libubifocus.so)
-            :&
         vendor/lib/libts_detected_face_hal.so)
             ;&
         vendor/lib/libts_face_beautify_hal.so)
             "${PATCHELF}" --replace-needed "libstdc++.so" "libstdc++_vendor.so" "${2}"
             ;;
     esac
+
+    # For all ELF files
+    if [[ "${1}" =~ ^.*(\.so|\/bin\/.*)$ ]]; then
+        "${PATCHELF_0_18}" --replace-needed "libstdc++.so" "libstdc++_vendor.so" "${2}"
+    fi
 }
 
 # If we're being sourced by the common script that we called,
