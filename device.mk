e20481ac6e7572ceff2f068e331ed5681c214178
Itz-Kanasama <164613153+Itz-Kanasama@users.noreply.github.com>
2025-04-23 11:33:53 +0000

    vince: Build missing libraries for 14 QPR3
    Co-authored-by: RennAlt <shenprjktplayground@gmail.com>

--- device.mk
+++ device.mk
@@ -37,6 +37,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3-service.xiaomi
 
+PRODUCT_PACKAGES += \
+     liblzma.vendor:64
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
